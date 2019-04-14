c82726574b0a05ea951f32335476a2f9f73c2a98
Xiaoya Huang <xhuang329@gatech.edu>
2019-04-13 21:26:58 -0400

    First commit

---
+++ gitCollaboration.java
@@ -0,0 +1,18 @@
+
+public class gitCollaboration {
+	String name;
+	String email;
+
+	gitCollaboration(String studentName, String studentEmail) {
+		name = studentName;
+		email = studentEmail;
+	}
+
+	public static void main(String[] args) {
+		// my info
+		new gitCollaboration("Xiaoya", "xiaoyah@seas.upenn.edu");
+		// my collaborator info
+		// please feel free to add anything you'd like to share!
+
+	}
+}
